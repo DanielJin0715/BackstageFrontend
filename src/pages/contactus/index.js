a5254696c5b978bdc74dca23ebad779c9326396d
Cocoa Dev <babypigdev@gmail.com>
2022-04-21 19:04:02 +0900

    add emailJS

--- src/pages/contactus/index.js
+++ src/pages/contactus/index.js
@@ -1,91 +1,55 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useRef } from "react";
+// import axios from "axios";
+import emailjs from "@emailjs/browser";
 import Footer from "../../components/footer";
 import Header from "../../components/header";
 import styles from "./index.module.css";
 
-// const api = axios.create();
-
-// api.interceptors.request.use(
-//   (request) => {
-//     let baseURL = "http://localhost:3000";
-//     request.url = baseURL + request.url;
-//     return request;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-
-function Form({ submit }) {
-  const [name, setName] = useState("");
-  const [surName, setSurName] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [email, setEmail] = useState("");
-  const [organization, setOrganization] = useState("");
-  const [message, setMessage] = useState("");
-
-  const onSubmit = () => {
-    submit({ name, surName, phoneNumber, email, organization, message });
-  };
-
+function Form({ form, submit }) {
   return (
     <div className={styles.form_wrapper}>
       <div className={styles.form}>
         <div className={styles.form_title}>Contact Us</div>
-        <div className={styles.form_group}>
-          <div className={styles.input_sect}>
-            <div className={styles.form_label}>Name</div>
-            <input
-              className={styles.form_input}
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
+        <form ref={form}>
+          <div className={styles.form_group}>
+            <div className={styles.input_sect}>
+              <div className={styles.form_label}>Name</div>
+              <input className={styles.form_input} type="text" name="name" />
+            </div>
+            <div className={styles.input_sect}>
+              <div className={styles.form_label}>Surmame</div>
+              <input className={styles.form_input} type="text" name="surName" />
+            </div>
+            <div className={styles.input_sect}>
+              <div className={styles.form_label}>Phone Number</div>
+              <input
+                className={styles.form_input}
+                type="text"
+                name="phoneNumber"
+              />
+            </div>
+            <div className={styles.input_sect}>
+              <div className={styles.form_label}>Email</div>
+              <input className={styles.form_input} type="email" name="email" />
+            </div>
+            <div className={styles.input_sect}>
+              <div className={styles.form_label}>Organization</div>
+              <input
+                className={styles.form_input}
+                type="text"
+                name="organization"
+              />
+            </div>
+            <div className={styles.input_sect}>
+              <div className={styles.form_label}>Message</div>
+              <textarea className={styles.form_textarea} name="message" />
+            </div>
+
+            <div className={styles.form_submit} onClick={submit}>
+              SUBMIT
+            </div>
           </div>
-          <div className={styles.input_sect}>
-            <div className={styles.form_label}>Surmame</div>
-            <input
-              className={styles.form_input}
-              value={surName}
-              onChange={(e) => setSurName(e.target.value)}
-            />
-          </div>
-          <div className={styles.input_sect}>
-            <div className={styles.form_label}>Phone Number</div>
-            <input
-              className={styles.form_input}
-              value={phoneNumber}
-              onChange={(e) => setPhoneNumber(e.target.value)}
-            />
-          </div>
-          <div className={styles.input_sect}>
-            <div className={styles.form_label}>Email</div>
-            <input
-              className={styles.form_input}
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className={styles.input_sect}>
-            <div className={styles.form_label}>Organization</div>
-            <input
-              className={styles.form_input}
-              value={organization}
-              onChange={(e) => setOrganization(e.target.value)}
-            />
-          </div>
-          <div className={styles.input_sect}>
-            <div className={styles.form_label}>Message</div>
-            <textarea
-              className={styles.form_textarea}
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            />
-          </div>
-          <div className={styles.form_submit} onClick={onSubmit}>
-            SUBMIT
-          </div>
-        </div>
+        </form>
       </div>
       <Footer />
     </div>
@@ -93,40 +57,42 @@ function Form({ submit }) {
 }
 
 function ContacUs() {
-  const submit = ({
-    name,
-    surName,
-    phoneNumber,
-    email,
-    organization,
-    message,
-  }) => {
+  const form = useRef();
+
+  const submit = () => {
     console.log("request sent");
-    return new Promise((resolve, reject) => {
-      axios
-        .post(
-          "http://ec2-3-143-0-249.us-east-2.compute.amazonaws.com:3000/api/auth/send_email",
-          {
-            name,
-            surName,
-            phoneNumber,
-            email,
-            organization,
-            message,
-          }
-        )
-        .then((res) => {
-          console.log(res.data);
-          // if (res.data.success === true) {
-          alert("Registered successfully");
-          // }
-          resolve();
-        })
-        .catch((err) => {
-          console.log(err);
-          reject();
-        });
-    });
+    emailjs
+      .sendForm(
+        "service_erw2ld9",
+        "template_75tt6ir",
+        form.current,
+        "-dCe7JmPnoBko-5QM"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("Successfully registered!");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    emailjs
+      .sendForm(
+        "service_erw2ld9",
+        "template_v29fwqf",
+        form.current,
+        "-dCe7JmPnoBko-5QM"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("Successfully registered!");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
   return (
     <div
@@ -137,7 +103,7 @@ function ContacUs() {
       }}
     >
       <Header />
-      <Form submit={submit} />
+      <Form submit={submit} form={form} />
     </div>
   );
 }
