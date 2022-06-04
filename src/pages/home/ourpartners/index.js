7aa12c8fe78402486a4560b553984f6e763ad2ec
Daniel Jin <danieljin0715@gmail.com>
2022-06-04 17:50:16 +0900

    add partners

--- src/pages/home/ourpartners/index.js
+++ src/pages/home/ourpartners/index.js
@@ -3,12 +3,10 @@ import React from "react";
 import { useGlobalStateContext } from "../../../context";
 import styles from "./index.module.css";
 
-const partners = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-];
-
 function Partners({ isMobile }) {
+  const partners = [];
+  for (let i = 1; i <= 48; i++) partners.push(i);
+
   return (
     <div className={styles.partners}>
       {partners.map((partner) => {
